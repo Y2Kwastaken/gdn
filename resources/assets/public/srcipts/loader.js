7f4325d2746101e4759fb1b8c945df676a1d218e
Y2Kwastaken <mwholder@proton.me>
2025-11-11 20:54:42 -0600

    Compentize some more stuff

--- resources/assets/public/srcipts/loader.js
+++ resources/assets/public/srcipts/loader.js
@@ -1,16 +1,7 @@
-var EDUCATION = document.getElementById("education-block");
-
-/*
-*         <div class="content-general">
-            <div class="content-general-header">
-                <a href="https://google.com" target="_blank" rel="noopener noreferrer"> <!-- Change cursor also -->
-                    <img src="imgs/college.png" />
-                </a>
-                <h3>School Institution 1</h3>
-            </div>
-            <p>Bachelors of Science, Computer Science | 20XX-20XX</p>
-        </div>
-*/
+var EDUCATION_BLOCK = document.getElementById("education-block");
+var MISSION_BLOCK = document.getElementById("mission-block");
+var JOB_BLOCK = document.getElementById("job-block");
+var CHARITY_BLOCK = document.getElementById("charity-block");
 
 function make_content(append_to, title_string, link_string, image, description_string_iterable) {
     let parent_node = document.createElement("div");
@@ -33,6 +24,15 @@ function make_content(append_to, title_string, link_string, image, description_s
     append_to.appendChild(parent_node);
 }
 
+function load_mission(json_obj) {
+    let fragment = document.createDocumentFragment();
+    let p = document.createElement("p");
+    p.textContent = json_obj.text;
+    fragment.appendChild(p);
+
+    MISSION_BLOCK.appendChild(fragment);
+}
+
 function load_education(json_obj) {
     let fragment = document.createDocumentFragment();
     json_obj.forEach(school_entry => {
@@ -40,23 +40,51 @@ function load_education(json_obj) {
         make_content(fragment, school_entry.name, school_entry.link, school_entry.image, [line]);
     })
 
-    EDUCATION.appendChild(fragment);
+    EDUCATION_BLOCK.appendChild(fragment);
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const cached = localStorage.getItem("education-data");
+function load_jobs(json_obj) {
+    let fragment = document.createDocumentFragment();
+    json_obj.forEach(job_entry => {
+        make_content(fragment, job_entry.company, job_entry.link, job_entry.image, [job_entry.role, job_entry.attended, job_entry.description, job_entry.experience_gained]);
+    })
+
+    JOB_BLOCK.appendChild(fragment);
+}
+
+function load_charities(json_obj) {
+    let fragment = document.createDocumentFragment();
+    json_obj.forEach(charity_entry => {
+        make_content(fragment, charity_entry.name, charity_entry.link, charity_entry.image, [charity_entry.description, charity_entry.contribution]);
+    });
+
+    CHARITY_BLOCK.appendChild(fragment);
+}
+
+async function cache_or_load(cache_name, json_file, load_function) {
+    const cached = localStorage.getItem(cache_name);
     if (cached) {
-        load_education(JSON.parse(cached));
+        load_function(JSON.parse(cached));
+        // force update (later This should happen less often maybe only every 30 minutes or so, this could also be done lazily by just free-ing the cache)
+        const response = await fetch(json_file, { cache: "force-cache" });
+        const data = await response.json();
+        localStorage.setItem(cache_name, JSON.stringify(data));
         return;
     }
 
-    // The preload ensures this returns nearly instantly
     try {
-        const response = await fetch("data/schools.json", { cache: "force-cache" });
+        const response = await fetch(json_file, { cache: "force-cache" });
         const data = await response.json();
-        load_education(data);
-        localStorage.setItem("education-data", JSON.stringify(data));
+        load_function(data);
+        localStorage.setItem(cache_name, JSON.stringify(data));
     } catch (error) {
-        console.error("Education Load Error:", error);
+        console.error("Failed to load website data: ", error);
     }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    cache_or_load("mission-data", "data/mission.json", load_mission);
+    cache_or_load("education-data", "data/schools.json", load_education);
+    cache_or_load("job-data", "data/jobs.json", load_jobs);
+    cache_or_load("charity-data", "data/charities.json", load_charities);
 });
